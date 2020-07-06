255344c877d2ce3dea3ae77ccbb9118fb2daa921
vbodia <bvasiliuk@gmail.com>
2020-07-06 17:27:41 +0300

    first r

---
+++ autoria.py
@@ -0,0 +1,42 @@
+import requests   
+from bs4 import BeautifulSoup
+
+URL = 'https://auto.ria.com/newauto/marka-jeep/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36', 'accept': '*/*'}
+HOST = 'https://auto.ria.com'
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='proposition_area')
+    
+    cars = []
+    for item in items:
+        uah_price = item.find('span', class_='grey size13')
+        if uah_price:
+            uah_price = uah_price.get_text()
+        else:
+            uah_price = "Уточняйте цену"
+
+        cars.append({
+            'title' : item.find('strong').get_text(strip=True),
+            'link': HOST + item.find('a').get('href'),
+            'usd_price': item.find('span', class_='green').get_text(strip=True),
+            'uah_price': uah_price,
+            'city': item.find('div', class_='proposition_region').find_next('strong').get_text(strip=True)
+        })
+    print(cars)
+
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        cars = get_content(html.text)
+    else:
+        print('error')
+
+parse()
